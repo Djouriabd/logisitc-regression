bf2d8de3d9dc67b036a18afe35f29d38a5c7c1ec
Djouriabd <148075963+Djouriabd@users.noreply.github.com>
2025-01-01 16:26:35 +0100

    Update logistic.c

--- logistic.c
+++ logistic.c
@@ -2,48 +2,50 @@
 #include<stdlib.h>
 #include<math.h>
 
-//float linear_regression(float w,float b,int x){
-int linear_regression(int w,int b,int x){
+float linear_regression(float w,float b,float x){
+//int linear_regression(int w,int b,int x){
 return (w*x)+b;}
 
-float logistic(int x) {
-    return 1 / (1 + exp(-x));
-}
+float logistic(float x) {
+    return 1.0/(1.0 + exp(-x));}
 
 float odd(float y){
     if (y==1||y==0) {return -1;}
     else{
         return y/(1-y);
-    }
-}
+    }}
 
 float log_odd(float z){
-    return log(z);
-}
+    return log(z);}
 
 int main(){
-    //float t1[20],t2[20],y;
-    int t1[20],t2[20],y;
-    for (int i=0;i<10;i++){
+    //i took n=20 for the both table to use a sample of 20 lines with 2 columns (the one of csv of salaries)
+    float t1[20],t2[20],y;
+    //int t1[20],t2[20],y;
+    for (int i=0;i<20;i++){
         printf("give me the values of the first table:");
-        scanf("%d",&t1[i]);
+        scanf("%f",&t1[i]);
     }
-    for ( i=0;i<10;i++){
+    for (i=0;i<20;i++){
         printf("give me the values of the second table:");
-        scanf("%d",&t2[i]);
+        scanf("%f",&t2[i]);
     }
     printf("give me the prediction number u want:");
-    scanf("%d",&y);
-    for ( i=0;i<10;i++){
-        //float fx=linear_regression(t1[i],t2[i],y);
-        int fx=linear_regression(t1[i],t2[i],y);
-        printf("\nthe prediction for the value:%d",fx);
+    scanf("%f",&y);
+    for (i=0;i<20;i++){
+        float fx=linear_regression(t1[i],t2[i],y);
+        //int fx=linear_regression(t1[i],t2[i],y);
+        printf("\nthe prediction for the value:%f",fx);
         float z=logistic(fx);
         printf("\nthe logistic for the value:%f",z);
         float k=odd(z);
-        printf("\nthe odd value:%f",k);
-        float j=log_odd(k);
-        printf("\nthe log odd value:%f",j);
+        if (k ==-1){
+            printf("can't calculate the odd it took -infinity value");}
+        else{
+            printf("\nthe odd value:%f",k);
+            float j=log_odd(k);
+            printf("\nthe log odd value:%f",j);
+        }
         printf("\n+--------------------------------+");
     }
     return 0;
